48a76b01df68ae685a0bba070afcfc9efd4028ee
MaxKnyazev <max.knyazev77@gmail.com>
2024-04-16 16:55:54 +0300

    feat: add generateTableRow()

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
   const log = console.log;
   log('DOMContentLoaded');
   
-  const h1 = document.getElementById('h1');
-  log(h1);
+  const tableBody = document.getElementById('table__body');
+  log(tableBody);
 
+  const generateTableRow = () => {
+    // Создаем строку таблицы
+    const tableRow = document.createElement('tr');
 
+    // Создаем 5 ячеек таблицы и добавляем их в строку
+    for (let i = 0; i < 5; i++) {
+      const cell = document.createElement('td');
+      cell.textContent = `content-${i}`;
+      tableRow.appendChild(cell);
+    }
+
+    // Возвращаем строку таблицы
+    return tableRow;
+  }
+
+  // Вызываем функцию и выводим результат в консоль
+  console.log(generateTableRow());
+  tableBody.appendChild(generateTableRow())
 
 
 
