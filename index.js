80f45dfdabe27a1489286858dfd6d21dda7d58ad
MaxKnyazev <max.knyazev77@gmail.com>
2024-04-16 22:27:41 +0300

    feat: add test renders

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ document.addEventListener('DOMContentLoaded', async() => {
   const SELECT = 'username,age,gender,email,phone,address';
   const URL_FAKE_API = `https://dummyjson.com/users?limit=${LIMIT}&select=${SELECT}`;
   let users = [];
-  const tableBody = document.getElementById('table__body');
+  let table = document.getElementById('table');
+  let tableBody = document.getElementById('table__body');
   // console.log(tableBody);
-  let pageState = 1;
+  let pageState = 0;
 
 
 
@@ -43,9 +44,14 @@ document.addEventListener('DOMContentLoaded', async() => {
 
   //------------------------------------------------------------------ renderTablePage
   const renderTablePage = (arr, firstIndex, lastIndex) => {
+    tableBody.remove();
+    tableBody = document.createElement('tbody');
+
     for (let i = firstIndex; i <= lastIndex; i++) {
       tableBody.appendChild(generateTableRow(arr[i]))
     }
+    log(tableBody)
+    table.appendChild(tableBody)
   }
 
 
@@ -67,92 +73,10 @@ document.addEventListener('DOMContentLoaded', async() => {
 
   log('users = ',users)
 
-  renderTablePage(users, pageState - 1, pageState + 8);
-
-  pageState = 2;
-  setTimeout(() => {renderTablePage(users, pageState - 1, pageState + 8);}, 2000)
+//******************************************  Проверка рендера постранично  */
+  renderTablePage(users, pageState * 10, pageState * 10 + 9);
+  setTimeout(() => {pageState = 1;renderTablePage(users, pageState * 10, pageState * 10 + 9)}, 2000)
+  setTimeout(() => {pageState = 2;renderTablePage(users, pageState * 10, pageState * 10 + 9)}, 4000)
+  setTimeout(() => {pageState = 3;renderTablePage(users, pageState * 10, pageState * 10 + 9)}, 6000)
 
   });
-
-
-
-
-  /**
-   * 
-   * 
-   * https://dummyjson.com/users?limit=100&select=username,age,gender,email,phone,address
-   * 
-   * 
-   * // 20240416182612
-  // https://dummyjson.com/users
-  
-  {
-    "users": [
-      {
-        "id": 1,
-        "firstName": "Terry",
-        "lastName": "Medhurst",
-        "maidenName": "Smitham",
-        "age": 50,
-        "gender": "male",
-        "email": "atuny0@sohu.com",
-        "phone": "+63 791 675 8914",
-        "username": "atuny0",
-        "password": "9uQFF1Lh",
-        "birthDate": "2000-12-25",
-        "image": "https://robohash.org/Terry.png?set=set4",
-        "bloodGroup": "A-",
-        "height": 189,
-        "weight": 75.4,
-        "eyeColor": "Green",
-        "hair": {
-          "color": "Black",
-          "type": "Strands"
-        },
-        "domain": "slashdot.org",
-        "ip": "117.29.86.254",
-        "address": {
-          "address": "1745 T Street Southeast",
-          "city": "Washington",
-          "coordinates": {
-            "lat": 38.867033,
-            "lng": -76.979235
-          },
-          "postalCode": "20020",
-          "state": "DC"
-        },
-        "macAddress": "13:69:BA:56:A3:74",
-        "university": "Capitol University",
-        "bank": {
-          "cardExpire": "06/22",
-          "cardNumber": "50380955204220685",
-          "cardType": "maestro",
-          "currency": "Peso",
-          "iban": "NO17 0695 2754 967"
-        },
-        "company": {
-          "address": {
-            "address": "629 Debbie Drive",
-            "city": "Nashville",
-            "coordinates": {
-              "lat": 36.208114,
-              "lng": -86.58621199999999
-            },
-            "postalCode": "37076",
-            "state": "TN"
-          },
-          "department": "Marketing",
-          "name": "Blanda-O'Keefe",
-          "title": "Help Desk Operator"
-        },
-        "ein": "20-9487066",
-        "ssn": "661-64-2976",
-        "userAgent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/12.0.702.0 Safari/534.24",
-        "crypto": {
-          "coin": "Bitcoin",
-          "wallet": "0xb9fc2fe63b2a6c003f1c324c3bfa53259162181a",
-          "network": "Ethereum (ERC20)"
-        }
-      },
-   * 
-   */
